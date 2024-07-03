1209f9063ecfce8914c784a83bfe637c361b0ded
federicotarantino <federico.tarantino@gmail.com>
2024-07-03 21:03:24 +0200

    commit locale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
   title = 'cisaro';
+  @ViewChild('mainInput') input!: ElementRef;
+  squadra: string = "";
+  frase: string = "";
+  timeout: any;
+
+  ngOnInit(){
+    this.squadra = "logo";
+    this.setFrase();
+  }
+
+  ngAfterViewInit() {
+    this.input.nativeElement.focus();
+  }
+
+  getClass() {
+    return "container-fluid "+this.squadra;
+  }
+
+  scegliSquadra() {
+    this.squadra = atob(this.input.nativeElement.value);
+    this.setFrase();
+    this.input.nativeElement.value = "";
+    if(this.timeout){
+      clearTimeout(this.timeout);
+    }
+    this.timeout = setTimeout(()=>{
+      this.squadra = "logo";
+      this.setFrase();
+    }, 10000);
+  }
+
+  setFrase(){
+    if(this.squadra=="logo"){
+      this.frase = "Verifica Carta";
+    } else if(this.squadra=="chef"){
+      this.frase = "Sei nella BRIGATA CHEF";
+    } else if(this.squadra=="bimbi"){
+      this.frase = "Sei un Toad aiutante con tanto TEMPO LIBERO";
+    } else {
+      this.frase = "Sei nel gruppo "+this.squadra.toUpperCase();
+    }
+  }
 }
